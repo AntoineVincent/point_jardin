53ef8ec6489eb90fb0a653bbe1ad604472b27391
Antoine <anton51200@laposte.net>
2016-03-24 10:12:20 +0100

    ajout d'un include, modification en php

---
+++ header.php
@@ -0,0 +1,28 @@
+<div class="row">
+	<div class="col-md-1"></div>
+	<div class="col-md-2">
+		<p class="gauche menu">
+			<a href="./page2.html">PRODUITS</a>
+		</p>
+	</div>
+	<div class="col-md-2">
+		<p class="midgauche menu">
+			<a href="./page3.html">SERVICES</a>
+		</p>
+	</div>
+	<div class="col-md-3">
+		<p>
+			<img src='./images/logo.jpg' alt="logo"></br>
+		</p>
+	</div>
+	<div class="col-md-2">
+		<p class="middroite menuu">
+			<a href="./page4.html">ACTUS</a>
+		</p>
+	</div>
+	<div class="col-md-2">
+		<p class="droite menuu">
+			<a href="./page5.html">HORAIRES<a>
+		</p>
+	</div>
+</div>
