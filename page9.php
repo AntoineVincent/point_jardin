b6e5daae5a061870339d3a22393d2c9017a93b2b
Antoine <anton51200@laposte.net>
2016-03-24 15:15:18 +0100

    creation api google

--- page9.php
+++ page9.php
@@ -14,7 +14,7 @@
 				?>
 		</header>
 		<section>
-			<img src='images/grande_carte.png' alt="carte">
+			<iframe src="https://static.parastorage.com/services/santa/1.1254.17/static/external/googleMap.html?address=48.750050%2C%201.262953&amp;addressInfo=Point%20Jardin&amp;language=en&amp;lat=48.75005&amp;long=1.2629530000000386&amp;mapInteractive=true&amp;mapType=ROADMAP&amp;showMapType=true&amp;showPosition=true&amp;showStreetView=true&amp;showZoom=true&amp;ts=1370" scrolling="no" data-reactid=".0.$SITE_ROOT.$desktop_siteRoot.$PAGES_CONTAINER.1.1.$SITE_PAGES.$cluw.1.$GglMp0.2.0" frameborder="0" height="350px" width="800px"></iframe>
 		</section>
 		<footer>
 				<?php
