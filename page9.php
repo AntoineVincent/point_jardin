c5a8529b78946a9b50f334ae8bd732ec974130d1
keny700 <fredericbouillere@yahoo.fr>
2016-03-24 15:17:04 +0100

    page9

--- page9.php
+++ page9.php
@@ -4,17 +4,21 @@
 		<meta charset="utf-8" />
 		<script type="text/javascript" src="./js/bootstrap.min.js"></script>
 		<link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css">
-		<link rel="stylesheet" href="./css/style.css">	
+		<link rel="stylesheet" href="./css/style.css">
+		<link rel="stylesheet" href="./css/style9.css">	
+
 		<title>Carte</title>
+		
+
 	</head>
-	<body>
+	<body onload="initialize()">
 		<header>
 			<?php
 					include('./header.php')
 				?>
 		</header>
 		<section>
-			<img src='images/grande_carte.png' alt="carte">
+			<img src="images/grande_carte.png" alt="map">
 		</section>
 		<footer>
 				<?php
