58ce772de75180f751f3f26eed2dd4c34d7ca60b
keny700 <fredericbouillere@yahoo.fr>
2016-03-24 10:32:48 +0100

    footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+<div class="row">
+					<div class="col-md-4"> Copyright © 2016 </div>
+					<div class="col-md-2 newsletter"> Newsletter </div>
+					<div class="col-md-4 builtby"> Built by Fred et Anton</div>
+					<div class="col-md-2 footcont"> tel et contact .</div>
+				</div>
